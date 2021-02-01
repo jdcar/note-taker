99b06ecf6f4b15a7b47a2e49ccbdd1d6484eb0c4
jdcar <jdcarlstone@gmail.com>
2021-02-01 16:15:13 -0600

    Comment in homework requirements for setting up routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 const path = require('path')
 
+const db = require('db.json')
+
 // Copy/Paste this into all projects
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -13,8 +15,7 @@ app.use(express.json())
 // 
 // GET /notes - Should return the notes.html file.
 
-
-app.get('/add', (req, res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/notes.html'))
   console.log(path.join(__dirname + '/public/notes.html'))
 })
@@ -23,6 +24,29 @@ app.get('/add', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'))
 })
+
+// The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
+
+// GET /api/notes - 
+
+app.get('/api/notes', (req, res) => {
+    // Should read the db.json file and return all saved notes as JSON.
+})
+
+app.post('/api/notes', (req, res) => {
+   
+    // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    res.status(200).send()
+
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file
+})
+
+
+
 
 
 
