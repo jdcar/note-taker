7e83e7dd9bd09584ff1c138c40c19b82f4c91e7d
jdcar <jdcarlstone@gmail.com>
2021-02-02 09:53:54 -0600

    Add id to json and res.json to server

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // Express
-const express = require('express')
+const express = require('express');
+const fs = require('fs')
 const app = express()
 const PORT = process.env.PORT || 3000;
-
 const path = require('path')
+const db = require('./db/db.json')
 
-// const db = require('db\\db.json')
 
 // Copy/Paste this into all projects
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-// 
+// Set up
 // GET /notes - Should return the notes.html file.
 
 app.get('/notes', (req, res) => {
@@ -20,31 +20,38 @@ app.get('/notes', (req, res) => {
   console.log(path.join(__dirname + '/public/notes.html'))
 })
 
-//C:\Users\jcs8769\Desktop\bootcamp\homework\note-taker\public\notes.html
+// Set up
 // GET * - Should return the index.html 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'))
+  console.log(path.join(__dirname + '/public/index.html'))
+
 })
 
+// Done located at /db.json
 // The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
+
 
 // GET /api/notes - 
 
 app.get('/api/notes', (req, res) => {
-    // Should read the db.json file and return all saved notes as JSON.
+  // Should read the db.json file and return all saved notes as JSON.
+  res.json(db) 
+
 })
 
 app.post('/api/notes', (req, res) => {
-   
-    // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-    res.status(200).send()
+
+
+  // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+  res.status(200).send()
 
 })
 
 app.delete('/api/notes/:id', (req, res) => {
 
-    // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file
+  // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file
 })
 
 
@@ -53,7 +60,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
 
