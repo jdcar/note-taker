a428a3d0f7afd13d0568c3034074f51afac14b0a
jdcar <jdcarlstone@gmail.com>
2021-02-04 08:48:11 -0600

    Formatting changes to server file

--- server.js
+++ server.js
@@ -8,26 +8,14 @@ const PORT = process.env.PORT || 3000;
 const path = require('path')
 const db = require('./db/db.json')
 
-
-
-// Copy/Paste this into all projects
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
-
-
-// Set up
-// GET /notes - Should return the notes.html file.
-
-
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/notes.html'))
   console.log(path.join(__dirname + '/public/notes.html'))
 })
-
-// Set up
-// GET * - Should return the index.html 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'))
@@ -35,30 +23,17 @@ app.get('/', (req, res) => {
 
 })
 
-// Done located at /db.json
-// The application should have a db.json file on the backend that will be used to store and retrieve notes using the fs module.
-
-
-// GET /api/notes - 
-
 app.get('/api/notes', (req, res) => {
-  // Should read the db.json file and return all saved notes as JSON.
-
-
   res.json(db)
 
 })
 
-
 app.get('/api/notes/:id', (req, res) => {
   console.log(req.params)
   res.end()
-
 })
 
 app.post('/api/notes', (req, res) => {
-
-
   const newNote = req.body
   newNote.id = Math.floor(Math.random() * 1000000000);
   db.push(newNote)
@@ -67,17 +42,10 @@ app.post('/api/notes', (req, res) => {
     if (err) throw err;
     console.log('Saved!');
   });
-  // console.log(db)
-  // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
   res.status(200).send()
-  // res.json(db)
 })
 
-
-
 app.delete('/api/notes/:id', (req, res) => {
-
   let chosenNote = req.params.id
 
   fs.readFile('./db/db.json', 'utf8', (err, db) => {
@@ -100,10 +68,8 @@ app.delete('/api/notes/:id', (req, res) => {
 
       // console.log(notes)
     });
-    
   })
 })
-
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
